38d8613db2ef0110fe741f244c61e15427c33855
STIVEN MELENDEZ <stivenrafael@hotmail.com>
2019-08-23 14:44:34 -0500

    holi

---
+++ miapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+
+from django.urls import reverse_lazy
+
+from .models import Libro
+
+def cargar_inicio(request):
+    return render(request, "miapp/index.html")
+
+class LibroList(ListView):
+    model = Libro
+    template_name = 'miapp/lista_libros.html'
+
+class LibroCreate(CreateView):
+    model = Libro
+    fields = ['nombre','descripcion','isbn','copias']
+    template_name = 'miapp/nuevo_libro.html'
+    success_url = reverse_lazy('listar_libros')
+
+
